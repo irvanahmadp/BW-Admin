1b24854cfda04f2fd66bba9c7122ac7736b4ea84
IrvanAhmadPrasetya <irvan.ap1618@gmail.com>
2018-01-16 15:58:38 +0700

    Update graphics

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,23 +3,29 @@ $(document).ready(function(){
 	var dataChart={
 		labels: ["January", "February", "March", "April", "May", "June"],
 		datasets: [{
-			label: '# of session',
-			data: [19, 25, 99, 115, 175, 217],
-			borderWidth: 1,
+			label: 'session',
+			data: [19, 25, 99, 335, 175, 217],
+			borderWidth: 1.5,
 			/*backgroundColor: "#007bff"*/
-			backgroundColor: "rgba(0, 123, 255, 0.75)"
+			borderColor: "rgba(0, 123, 255, 1)",
+			backgroundColor: "rgba(0, 123, 255, 0.5)",
+			fill: false
 		},{
-			label: '# of users',
-			data: [19, 25, 99, 335, 497, 521],
-			borderWidth: 1,
+			label: 'users',
+			data: [19, 25, 99, 115, 497, 521],
+			borderWidth: 1.5,
 			/*backgroundColor: "#28a745"*/
-			backgroundColor: "rgba(40, 167, 69, 0.75)"
+			borderColor: "rgba(40, 167, 69, 1)",
+			backgroundColor: "rgba(40, 167, 69, 0.5)",
+			fill: false
 		},{
-			label: '# of views',
+			label: 'views',
 			data: [19, 25, 99, 512, 791, 719],
-			borderWidth: 1,
+			borderWidth: 1.5,
 			/*backgroundColor: "#17a2b8"*/
-			backgroundColor: "rgba(23, 162, 184, 0.75)"
+			borderColor: "rgba(23, 162, 184, 1)",
+			backgroundColor: "rgba(23, 162, 184, 0.5)",
+			fill: false
 		}]
 	};
 	var myChart = new Chart(chart1, {
